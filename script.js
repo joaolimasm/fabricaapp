a6c724c5e4f5596dc5c373522e3fff8033193f3e
Joao Victor <jv.limasm@gmail.com>
2020-09-16 12:54:04 -0300

    requisição

--- script.js
+++ script.js
@@ -1,31 +1,11 @@
-/* function entrar() {
-
-    var area = document.getElementById('login');
-    var text = prompt('Digite seu nome');
-
-    if (text == '' || text == null) {
-        alert('Digite seu nome novamente');
-
-        area.innerHTML = 'Bem vindo... ';
-    } else {
-        area.innerHTML = 'Bem vindo ' + text;
-    }
+function focado() {
+    console.log("Campo focado");
 }
 
-function entrar2(nome) {
-
-    var area = document.getElementById('login2');
-    var text = prompt('Digite seu sobrenome');
-    area.innerHTML = nome + " " + text;
-
-
-
+function desfocado() {
+    console.log("desfocou do campo");
 }
 
-function idade1(age1) {
-
-    var area = document.getElementById('idade');
-    var text = prompt('Digite Sua idade');
-    area.innerHTML = age1 + " " + text;
-
-} */
+function opcaoSelecionada(objeto) {
+    console.log('cidade selecionada: ' + objeto.value);
+}
