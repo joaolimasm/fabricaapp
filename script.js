fbc7564b34a307e5a1ab5ff300f82dfbad76e47d
Joao Victor <jv.limasm@gmail.com>
2020-09-16 14:04:05 -0300

    jogo do balao

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-function focado() {
-    console.log("Campo focado");
+var total = 0;
+var cont = 0;
+
+function criarBalao() {
+    var balao = document.createElement("div");
+    balao.setAttribute("class", "balao");
+
+    var x = Math.floor(Math.random() * 600);
+    var y = Math.floor(Math.random() * 400);
+
+    balao.setAttribute("style", "left:" + x + "px;top:" + y + "px;");
+    balao.setAttribute("onclick", "estourar(this)");
+    document.body.appendChild(balao);
 }
 
-function desfocado() {
-    console.log("desfocou do campo");
+function estourar(objeto) {
+    document.body.removeChild(objeto);
+
+    total++;
+    var score = document.getElementById('total');
+    score.innerHTML = "balões estourados: " + total;
 }
 
-function opcaoSelecionada(objeto) {
-    console.log('cidade selecionada: ' + objeto.value);
+function carregarJogo() {
+    setInterval(criarBalao, 1000);
 }
