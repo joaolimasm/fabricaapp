ac164a54af9b7923f19ae027815d6413ccef9945
Joao Victor <jv.limasm@gmail.com>
2020-09-16 12:24:03 -0300

    commite

---
+++ script.js
@@ -0,0 +1,31 @@
+/* function entrar() {
+
+    var area = document.getElementById('login');
+    var text = prompt('Digite seu nome');
+
+    if (text == '' || text == null) {
+        alert('Digite seu nome novamente');
+
+        area.innerHTML = 'Bem vindo... ';
+    } else {
+        area.innerHTML = 'Bem vindo ' + text;
+    }
+}
+
+function entrar2(nome) {
+
+    var area = document.getElementById('login2');
+    var text = prompt('Digite seu sobrenome');
+    area.innerHTML = nome + " " + text;
+
+
+
+}
+
+function idade1(age1) {
+
+    var area = document.getElementById('idade');
+    var text = prompt('Digite Sua idade');
+    area.innerHTML = age1 + " " + text;
+
+} */
