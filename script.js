0473abf19595a613611dec57b9688132852dbf47
Joao Victor <jv.limasm@gmail.com>
2020-09-21 19:31:55 -0300

    praticando classes

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-function cadastrar(usuarios, ...novosusuarios) {
-    let totalUsuarios = [
-        ...usuarios,
-        ...novosusuarios
+class Pessoa {
+    constructor() {
+        this.nome = '',
+            this.sobrenome = ''
+    }
+    nomePessoa(primeironome) {
+        this.nome = primeironome;
+        console.log('Meu nome é: ' + this.nome);
+    }
+    segundoNome(segundoNome) {
+        this.sobrenome = segundoNome;
+        console.log('Meu sobrenome é: ' + this.sobrenome);
+    }
+    nomeCompleto() {
+        let nomeCompleto = this.nome + ' ' + this.sobrenome;
+        console.log(nomeCompleto);
+    }
 
-    ];
-    return console.log(totalUsuarios);
+};
 
-}
-
-let usuarios = ["jhon", "magalhaes"];
-
-let novosusuarios = cadastrar(usuarios, "joao", "lima");
+let pessoa1 = new Pessoa();
+pessoa1.nomePessoa('Jhon');
+pessoa1.segundoNome('Magalhães');
+pessoa1.nomeCompleto();
